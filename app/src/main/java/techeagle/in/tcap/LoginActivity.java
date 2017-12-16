95c946b32285547a97b516dbb1de220d7a9a915b
durzoflint <abhinavu1201@gmail.com>
2017-12-16 20:59:16 +0530

    Completed Login

--- app/src/main/java/techeagle/in/tcap/LoginActivity.java
+++ app/src/main/java/techeagle/in/tcap/LoginActivity.java
@@ -1,7 +1,21 @@
 package techeagle.in.tcap;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +23,80 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        Button login = findViewById(R.id.login);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText username = findViewById(R.id.username);
+                EditText password = findViewById(R.id.password);
+                String id = username.getText().toString();
+                String pass = password.getText().toString();
+                new Login().execute(id, pass);
+            }
+        });
+    }
+    private class Login extends AsyncTask<String,Void,Void> {
+        String webPage="";
+        String baseUrl = "http://www.techeagle.in/tcap/";
+        ProgressDialog progressDialog;
+        @Override
+        protected void onPreExecute(){
+            progressDialog = ProgressDialog.show(LoginActivity.this, "Please Wait!","Logging In!");
+            super.onPreExecute();
+        }
+        @Override
+        protected Void doInBackground(String... strings){
+            URL url;
+            HttpURLConnection urlConnection = null;
+            try
+            {
+                url = new URL(handleSpaces(baseUrl+"login.php?username="+strings[0]+"&password="+strings[1]));
+                urlConnection = (HttpURLConnection) url.openConnection();
+                BufferedReader br=new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+                String data;
+                while ((data=br.readLine()) != null)
+                    webPage=webPage+data;
+            }
+            catch (IOException e)
+            {
+                e.printStackTrace();
+            }
+            finally
+            {
+                if (urlConnection != null)
+                    urlConnection.disconnect();
+            }
+            return null;
+        }
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+            progressDialog.dismiss();
+            if(webPage.contains("login successful<br>"))
+            {
+                int brI = webPage.indexOf("<br>");
+                webPage = webPage.substring(brI+4);
+                brI = webPage.indexOf("<br>");
+                String name = webPage.substring(0, brI);
+                webPage = webPage.substring(brI+4);
+                Intent i = new Intent(LoginActivity.this, HomeActivity.class);
+                i.putExtra("Name",name);
+                startActivity(i);
+            }
+            else
+                Toast.makeText(LoginActivity.this, "Username or Password incorrect.", Toast.LENGTH_LONG).show();
+        }
+    }
+    String handleSpaces(String s){
+        String x="";
+        for(int i=0;i<s.length();i++)
+        {
+            char ch= s.charAt(i);
+            if(ch==' ')
+                x=x+"%20";
+            else
+                x=x+ch;
+        }
+        return x;
     }
 }
