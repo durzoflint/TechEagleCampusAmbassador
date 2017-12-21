96cc6e788bcda6b914f352840d47d6dfc5497f1b
durzoflint <abhinavu1201@gmail.com>
2017-12-21 18:36:41 +0530

    Added firebase auth

--- app/src/main/java/techeagle/in/tcap/HomeActivity.java
+++ app/src/main/java/techeagle/in/tcap/HomeActivity.java
@@ -1,7 +1,10 @@
 package techeagle.in.tcap;
 
+import android.app.ProgressDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.os.AsyncTask;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
@@ -17,8 +20,26 @@ import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.firebase.ui.auth.AuthUI;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 public class HomeActivity extends AppCompatActivity {
+    //Uri xx = FirebaseAuth.getInstance().getCurrentUser().getPhotoUrl();
+    public static final int RC_SIGN_IN = 1;
+    private FirebaseAuth mFirebaseAuth;
+    private FirebaseAuth.AuthStateListener mAuthStateListener;
     private Boolean isFabOpen = false;
     FloatingActionButton fab1, fab2, fab3, fab4, fab5, fab6;
     private Animation fab_open, fab_close, fade_in, fade_out;
@@ -29,6 +50,34 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        mAuthStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    onSignedInInitialize(user.getDisplayName());
+                }
+                else
+                {
+                    onSignedOutCleanup();
+                    List<AuthUI.IdpConfig> providers = Arrays.asList(
+                            new AuthUI.IdpConfig.Builder(AuthUI.EMAIL_PROVIDER).build(),
+                            new AuthUI.IdpConfig.Builder(AuthUI.GOOGLE_PROVIDER).build()
+                    );
+
+                    startActivityForResult(
+                            AuthUI.getInstance()
+                                    .createSignInIntentBuilder()
+                                    .setIsSmartLockEnabled(false)
+                                    .setAvailableProviders(providers)
+                                    .setTheme(R.style.LoginTheme)
+                                    .setLogo(R.drawable.applogo)
+                                    .build(),
+                            RC_SIGN_IN);
+                }
+            }
+        };
         Intent i = getIntent();
         name = i.getStringExtra("Name");
         username = i.getStringExtra("Username");
@@ -138,17 +187,109 @@ public class HomeActivity extends AppCompatActivity {
         logout();
     }
     void logout(){
-        new AlertDialog.Builder(this)
+        AuthUI.getInstance().signOut(this);
+        /*new AlertDialog.Builder(this)
                 .setTitle("Really Logout?")
                 .setMessage("Are you sure you want to Logout?")
                 .setNegativeButton(android.R.string.no, null)
                 .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface arg0, int arg1) {
-                        Intent i = getBaseContext().getPackageManager()
+                        *//*Intent i = getBaseContext().getPackageManager()
                                 .getLaunchIntentForPackage( getBaseContext().getPackageName() );
                         i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                        startActivity(i);
+                        startActivity(i);*//*
                     }
-                }).create().show();
+                }).create().show();*/
     }
+    private void onSignedInInitialize(String username) {
+    }
+
+    private void onSignedOutCleanup() {
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mFirebaseAuth.addAuthStateListener(mAuthStateListener);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (mAuthStateListener != null)
+        {
+            mFirebaseAuth.removeAuthStateListener(mAuthStateListener);
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == RC_SIGN_IN)
+        {
+            if (resultCode == RESULT_OK)
+            {
+                Toast.makeText(this, "Signed in!", Toast.LENGTH_SHORT).show();
+            }
+            else if (resultCode == RESULT_CANCELED) {
+                Toast.makeText(this, "Sign in canceled", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    /*private class Login extends AsyncTask<String,Void,Void> {
+        String webPage="";
+        String baseUrl = "http://www.techeagle.in/tcap/";
+        ProgressDialog progressDialog;
+        @Override
+        protected void onPreExecute(){
+            progressDialog = ProgressDialog.show(HomeActivity.this, "Please Wait!","Logging In!");
+            super.onPreExecute();
+        }
+        @Override
+        protected Void doInBackground(String... strings){
+            URL url;
+            HttpURLConnection urlConnection = null;
+            try
+            {
+                String myURL = baseUrl+"login.php?username="+strings[0]+"&password="+strings[1];
+                myURL = myURL.replaceAll(" ", "%20");
+                url = new URL(myURL);
+                urlConnection = (HttpURLConnection) url.openConnection();
+                BufferedReader br=new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+                String data;
+                while ((data=br.readLine()) != null)
+                    webPage=webPage+data;
+            }
+            catch (IOException e)
+            {
+                e.printStackTrace();
+            }
+            finally
+            {
+                if (urlConnection != null)
+                    urlConnection.disconnect();
+            }
+            return null;
+        }
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+            progressDialog.dismiss();
+            if(webPage.contains("login successful<br>"))
+            {
+                int brI = webPage.indexOf("<br>");
+                webPage = webPage.substring(brI+4);
+                brI = webPage.indexOf("<br>");
+                String name = webPage.substring(0, brI);
+                webPage = webPage.substring(brI+4);
+                Intent i = new Intent(HomeActivity.this, HomeActivity.class);
+                i.putExtra("Name",name);
+                i.putExtra("Username", id);
+                startActivity(i);
+            }
+            else
+                Toast.makeText(HomeActivity.this, "Username or Password incorrect.", Toast.LENGTH_LONG).show();
+        }
+    }*/
 }
