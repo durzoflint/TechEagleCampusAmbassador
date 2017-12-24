ea3f09d15662c639609c521d169b2803f0e79b8e
durzoflint <abhinavu1201@gmail.com>
2017-12-25 03:52:46 +0530

    App Released

--- app/src/main/java/techeagle/in/tcap/AboutAppActivity.java
+++ app/src/main/java/techeagle/in/tcap/AboutAppActivity.java
@@ -1,7 +1,11 @@
 package techeagle.in.tcap;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class AboutAppActivity extends AppCompatActivity {
 
@@ -10,5 +14,12 @@ public class AboutAppActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_app);
         setTitle("About TCAP");
+        TextView redirect = findViewById(R.id.redirect);
+        redirect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("http://techeagle.in/tcap/redirecttovideo.php")));
+            }
+        });
     }
 }
